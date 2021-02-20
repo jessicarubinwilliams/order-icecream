919a58d9a90b51bea5201ce39d8ec65e87eca2c2
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-02-19 16:41:00 -0800

    Add cone info to price method--broke mehthod.
    Co-authored-by: Sofia Acosta <sofiaacostarascon@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -10,26 +10,39 @@ IceCream.prototype.calculatePrice = function() {
   this.price += this.toppings.length
   const that = this;
   that.flavors.forEach(function(flavor) {
-    if (flavor == "marionberry cheesecake" || flavor == "salted caramel ganache" || flavor == "tahitian vanilla bean" ) {
+    if (flavor == "marionberry cheesecake" || flavor == "salted caramel ganache" || flavor == "tahitian vanilla bean") {
       that.price += 2.00
     } else if (flavor == "truffle honey" || flavor == "bluecheese pears" || flavor == "gold dust vanilla bean") {
       that.price += 3.00;
     }
   });
-  if (this.isKiddieCone == true) {
-    this.price /= 2 }
-  i  
-
-
+  if (this.cone == "waffle") {
+    this.price += 2.00;
+  } else if (this.cone == "chocolate dipped") {
+    this.price += 3.00;
+  } else if (this.isKiddieCone == true) { //this portion of the logic works alone but does not work when combined with everything else
+    this.price /= 2;
+  }
 }
 
 
 let flavors = ["marionberry cheesecake", "salted caramel ganache","tahitian vanilla bean", "truffle honey"];
 let toppings = ["chocolate chips", "nuts", "candy dots"];
-let iceCream = new IceCream(flavors, "waffle", toppings);
+let iceCream = new IceCream(flavors, "chocolate dipped", toppings);
 iceCream.isKiddieCone = true;
 iceCream.calculatePrice();
 console.log(iceCream);
+
+// 5 cone
+// 2 marionberry cheesecake
+// 2 salted carmel ganache
+// 2 tahitian vanilla bean
+// 3 truffle honey
+// 1 chocolate chips
+// 1 nuts
+// 1 candy dots
+// 3 chocolate dipped cone
+// /2 isKiddieCone
 
 //Regular cone (no price added)
 //Waffle cone
